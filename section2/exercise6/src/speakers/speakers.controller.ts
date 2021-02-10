af11228047c3cd8c34a5dd4744a6073e0e635691
Ely Lucas <ely@meta-tek.net>
2021-02-09 19:52:02 -0700

    section2 exercise 6 work

---
+++ section2/exercise6/src/speakers/speakers.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
+import { SpeakerEntity } from 'src/data/speaker.entity';
+
+@Controller('speakers')
+export class SpeakersController {
+
+  constructor(private speakersRepository: SpeakersRepository) { }
+
+  @Post()
+  create(@Body() speaker: unknown) {
+    const speakerEntity = Object.assign(new SpeakerEntity(), speaker);
+    speakerEntity.createdBy = 'admin';
+    return this.speakersRepository.create(speakerEntity);
+  }
+
+  @Get(':id')
+  get(@Param('id') idStr: string) {
+    const id = parseInt(idStr, 10);
+    return this.speakersRepository.get(id);
+  }
+
+  @Get()
+  getList(@Query('name') name?: string, @Query('hasSpokenBefore') hasSpokenBeforeStr?: string) {
+    let hasSpokenBefore: boolean;
+    if (hasSpokenBeforeStr === 'true') {
+      hasSpokenBefore = true;
+    } else if (hasSpokenBeforeStr === 'false') {
+      hasSpokenBefore = false;
+    }
+    return this.speakersRepository.getAll({ name, hasSpokenBefore });
+  }
+  
+}
